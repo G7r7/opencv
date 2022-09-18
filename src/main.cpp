9834b40eb5ec4461c0b1f79f976973869840204c
Germain Rullier <grullier@free.fr>
2022-09-18 15:03:18 +0000

    circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,110 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-cv::Mat hough(cv::Mat img)
+struct Circle {
+    size_t center_x;
+    size_t center_y;
+    size_t radius;
+};
+
+std::vector<Circle> hough(cv::Mat img)
 {
-    int nbCols = img.cols;
     int nbRows = img.rows;
+    int nbCols = img.cols;
     int nbRadius = std::sqrt(img.cols * img.cols + img.rows * img.rows);
     int acc[nbRows][nbCols][nbRadius];
     int threshold = 1000;
 
-    std::cout << nbRows;
-
+    // Initialization
     for (size_t i = 0; i < nbRows; i++)
     {
         for (size_t j = 0; j < nbCols; j++)
         {
             for (size_t k = 0; k < nbRadius; k++)
             {
-                std::cout << i << std::endl;
-                std::cout << j << std::endl;
-                std::cout << k << std::endl;
                 acc[i][j][k] = 0;
             }
         }
     }
 
-    // for (size_t i = 0; i < img.rows; i++)
-    // {
-    //     for (size_t j = 0; j < img.cols; j++)
-    //     {
-    //         if (img.at<int>(i, j) > threshold)
-    //         {
-    //             for (size_t r = 0; r < img.rows; r++)
-    //             {
-    //                 for (size_t c = 0; c < img.cols; c++)
-    //                 {
-    //                     int delta_x = j - c;
-    //                     int delta_y = i - r;
-    //                     int radius = std::sqrt(delta_x * delta_x + delta_y * delta_y);
+    // Voting for circles
+    for (size_t i = 0; i < img.rows; i++)
+    {
+        for (size_t j = 0; j < img.cols; j++)
+        {
+            // For each "border" pixel 
+            if (img.at<int>(i, j) > threshold)
+            {
+                for (size_t r = 0; r < img.rows; r++)
+                {
+                    for (size_t c = 0; c < img.cols; c++)
+                    {
+                        int delta_x = j - c;
+                        int delta_y = i - r;
+                        int radius = std::sqrt(delta_x * delta_x + delta_y * delta_y);
 
-    //                     acc[r][c][radius] += 1;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+                        acc[r][c][radius] += 1;
+                    }
+                }
+            }
+        }
+    }
 
-    return img;
+    // Find most voted circle
+    size_t maximum_score = 0;
+    size_t max_row, max_col, max_rad = 0;
+    for (size_t i = 0; i < nbRows; i++)
+    {
+        for (size_t j = 0; j < nbCols; j++)
+        {
+            for (size_t k = 0; k < nbRadius; k++)
+            {
+                if (acc[i][j][k] > maximum_score)
+                {
+                    maximum_score = acc[i][j][k];
+                    max_row = i;
+                    max_col = j;
+                    max_rad = k;
+                }
+                
+            }
+            
+        }   
+    }
+
+    std::cout << maximum_score << std::endl;
+    std::cout << max_row << std::endl;
+    std::cout << max_col << std::endl;
+    std::cout << max_rad << std::endl;
+    
+    std::vector<Circle> circles;
+    circles.push_back(Circle{
+        max_row,
+        max_col,
+        max_rad
+    });
+
+    return circles;
 }
 
 int main(int argc, char **argv)
 {
     cv::Mat image; // variable image of datatype Matrix
-    image = cv::imread("../images/four.png");
+    image = cv::imread("../images/fourn.png");
 
-    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
 
-    cv::GaussianBlur(image, image, cv::Size(3, 3), 0);
+    cv::GaussianBlur(gray, gray, cv::Size(3, 3), 0);
 
     cv::Mat sobel;
-    cv::Sobel(image, sobel, -1, 1, 1);
+    cv::Sobel(gray, sobel, -1, 1, 1);
 
-    cv::Mat circles = hough(sobel);
+    std::vector<Circle> circles = hough(sobel);
+
+    for (auto circle : circles) {
+        cv::circle(image, cv::Point(circle.center_x, circle.center_y), circle.radius, cv::Scalar(0, 0, 255));
+    }
 
     cv::imshow("Display Image", image);
     cv::waitKey(0);
