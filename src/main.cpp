770f59c299b4441b6772a5031407ffa0eca0fd6b
Germain Rullier <grullier@free.fr>
2022-09-22 19:28:30 +0000

    Non working algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,21 @@ struct Circle {
     size_t radius;
 };
 
+struct Score {
+    float score;
+    size_t row;
+    size_t col;
+    size_t rad;
+};
+
 std::vector<Circle> hough(cv::Mat img)
 {
     int nbRows = img.rows;
     int nbCols = img.cols;
     int nbRadius = std::sqrt(img.cols * img.cols + img.rows * img.rows);
-    int acc[nbRows][nbCols][nbRadius];
-    int threshold = 1000;
+    int gradient_threshold = 1000;
+    
+    float acc[nbRows][nbCols][nbRadius];
 
     // Initialization
     for (size_t i = 0; i < nbRows; i++)
@@ -33,7 +41,7 @@ std::vector<Circle> hough(cv::Mat img)
         for (size_t j = 0; j < img.cols; j++)
         {
             // For each "border" pixel 
-            if (img.at<int>(i, j) > threshold)
+            if (img.at<int>(i, j) > gradient_threshold)
             {
                 for (size_t r = 0; r < img.rows; r++)
                 {
@@ -41,9 +49,14 @@ std::vector<Circle> hough(cv::Mat img)
                     {
                         int delta_x = j - c;
                         int delta_y = i - r;
-                        int radius = std::sqrt(delta_x * delta_x + delta_y * delta_y);
+                        double radius_raw = std::sqrt(delta_x * delta_x + delta_y * delta_y);
+                        int radius = round(radius_raw);
 
-                        acc[r][c][radius] += 1;
+                        if (radius >= 5) { // We ignore circles with radius below 5 pixels
+                            // We compensate the importance of bigger circle on smaller circles
+                            // by dividing the vote by their circonference
+                            acc[r][c][radius] += 1.f/(2*M_PI*radius);
+                        }
                     }
                 }
             }
@@ -51,38 +64,35 @@ std::vector<Circle> hough(cv::Mat img)
     }
 
     // Find most voted circle
-    size_t maximum_score = 0;
-    size_t max_row, max_col, max_rad = 0;
+    std::vector<Score> scores;
     for (size_t i = 0; i < nbRows; i++)
     {
         for (size_t j = 0; j < nbCols; j++)
         {
             for (size_t k = 0; k < nbRadius; k++)
             {
-                if (acc[i][j][k] > maximum_score)
-                {
-                    maximum_score = acc[i][j][k];
-                    max_row = i;
-                    max_col = j;
-                    max_rad = k;
-                }
-                
+                scores.push_back({acc[i][j][k], i, j, k});
             }
-            
         }   
     }
-
-    std::cout << maximum_score << std::endl;
-    std::cout << max_row << std::endl;
-    std::cout << max_col << std::endl;
-    std::cout << max_rad << std::endl;
-    
-    std::vector<Circle> circles;
-    circles.push_back(Circle{
-        max_row,
-        max_col,
-        max_rad
+    std::sort(scores.begin(), scores.end(), [](const Score& a, const Score& b){
+        // Biggest comes first
+        return a.score > b.score;
     });
+    
+    // We create a circle for each score
+    std::vector<Circle> circles;
+    int circles_limit = 4;
+    for (auto score : scores)
+    {
+        if (circles_limit == 0) { break; }
+        circles.push_back(Circle{
+            score.col,
+            score.row,
+            score.rad
+        });        
+        circles_limit -= 1;
+    }
 
     return circles;
 }
@@ -90,7 +100,7 @@ std::vector<Circle> hough(cv::Mat img)
 int main(int argc, char **argv)
 {
     cv::Mat image; // variable image of datatype Matrix
-    image = cv::imread("../images/fourn.png");
+    image = cv::imread("../images/four.png");
 
     cv::Mat gray;
     cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
